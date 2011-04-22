1695ae53666f8b272ae30a6e71396296a70ed713
Luis Perichon <luisperichon+github@gmail.com>
2011-04-22 20:27:37 -0300

    initial commit

---
+++ nat_personal.rb
@@ -0,0 +1,13 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'sinatra/base'
+require 'erb'
+
+class NatPersonalTrainerApp < Sinatra::Base
+  set :static, true
+  set :public, File.dirname(__FILE__) + '/static'
+
+  get '/20110421' do
+    erb(:newsletter_20110421)
+  end
+
+end
